25f7b1ef88665338f5db763cfca2eebaca7f1103
israfil <israfilgozudeli3@gmail.com>
2024-10-12 13:03:42 +0300

    dice roll

---
+++ lib/gradient_container.dart
@@ -0,0 +1,24 @@
+import 'package:first_app/dice_roller.dart';
+import 'package:flutter/material.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(colors: [
+          Color.fromARGB(255, 26, 2, 80),
+          Color.fromARGB(255, 85, 24, 171),
+        ], begin: startAlignment, end: endAlignment),
+      ),
+      child: const Center(
+        child: DiceRoller(),
+      ),
+    );
+  }
+}
